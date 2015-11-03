6e4bc98d19c1fff4a719d1d7f5a0efad018a51ae
Ravi P <ravip0711@gmail.com>
2015-11-02 20:11:01 -0700

    added resources customer

---
+++ db/migrate/20151103030806_create_customers.rb
@@ -0,0 +1,14 @@
+class CreateCustomers < ActiveRecord::Migration
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :phone
+      t.string :email
+      t.string :zip_code
+      t.date :birthday
+
+      t.timestamps
+    end
+  end
+end
