bbf1106e628fdf433faae3ff1c7acbd42fcc0fbd
Ravi P <ravip0711@gmail.com>
2015-11-02 23:01:41 -0700

    added faker gem, and faker into factories, created validations for customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ActiveRecord::Base
+
+  validates_presence_of :first_name, :last_name, :phone, :email, :zip_code
+  validates_uniqueness_of :phone, :email
+
 end
--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
   factory :customer do
-    first_name "MyString"
-last_name "MyString"
-phone "MyString"
-email "MyString"
-zip_code "MyString"
-birthday "2015-11-02"
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    phone { Faker::Number.number(10) }
+    email { Faker::Internet.email }
+    zip_code { Faker::Address.zip_code }
+    birthday { Faker::Date.backward(100) }
   end
-
 end
