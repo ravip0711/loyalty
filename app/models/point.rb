9be50d25d9428fedbe61bf571a4175a68abd09ec
Ravi <ravip0711@gmail.com>
2016-03-06 12:36:07 -0700

    moved styling div container for all the content to layout application view

--- app/models/point.rb
+++ app/models/point.rb
@@ -3,5 +3,5 @@ class Point < ActiveRecord::Base
 
   validates_presence_of :point_total
   validates :point_total, :numericality => {:only_integer => true}
-  validates_length_of :point_total, minimum: 1, maximum: 1000
+  validates_length_of :point_total, minimum: 1, maximum: 3
 end
